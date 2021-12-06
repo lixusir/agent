ce059682deb4f89cfd7f6a03801381937f9daafd
lx <2368889579@qq.com>
2021-12-06 17:30:14 +0800

    提交

--- addons/ewei_shopv2/core/model/member.php
+++ addons/ewei_shopv2/core/model/member.php
@@ -737,7 +737,9 @@ class Member_EweiShopV2Model {
                 'city' => !empty($mc['residecity']) ? $mc['residecity'] : $userinfo['city'],
                 'area' => $mc['residedist'],
                 'createtime' => time(),
-                'status' => 0
+                'status' => 1,
+                'isagent'   => 1,
+                'invete_code'   => invete_code(5)
             );
             pdo_insert('ewei_shop_member', $member);
             if(method_exists(m('member'),'memberRadisCountDelete')) {
